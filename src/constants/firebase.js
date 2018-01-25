608087aaaa6ebecc05bb00ea6e3f62db72ec6d03
mylnz <mustafa.yalnizz@gmail.com>
2018-01-25 19:56:55 +0300

    init

---
+++ src/constants/firebase.js
@@ -0,0 +1,16 @@
+// export default {
+//   apiKey: 'AIzaSyAHshJehJ-E_9AoUN3kfY8BLvZtFoXxFto',
+//   authDomain: 'react-native-starter-app.firebaseapp.com',
+//   databaseURL: 'https://react-native-starter-app.firebaseio.com',
+//   projectId: 'react-native-starter-app',
+//   storageBucket: 'react-native-starter-app.appspot.com',
+//   messagingSenderId: '762822550667',
+// };
+export default {
+  apiKey: "AIzaSyBH25YBtapv7jWG6gPtudEmuKZ7fUAF6ak",
+  authDomain: "rn-ex-pr.firebaseapp.com",
+  databaseURL: "https://rn-ex-pr.firebaseio.com",
+  projectId: "rn-ex-pr",
+  storageBucket: "rn-ex-pr.appspot.com",
+  messagingSenderId: "508618650477"
+};
